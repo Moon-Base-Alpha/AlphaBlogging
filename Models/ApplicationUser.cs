a7aabbbdcbd8c08370953d4d768cb9512351bf5a
chi.k.larsson@gmail.com <chi.k.larsson@gmail.com>
2021-12-16 11:44:53 +0100

    create ApplicationUser class

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlphaBlogging.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public override string Id { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string LastName { get; set; }
+    }
+}
