264586d12f8edc73358734be8ec1367ebcbd1a15
chi.k.larsson@gmail.com <chi.k.larsson@gmail.com>
2021-12-17 15:58:21 +0100

    create DbInitializer , admin

---
+++ Services/DbInitializer.cs
@@ -0,0 +1,52 @@
+﻿using AlphaBlogging.Data;
+using AlphaBlogging.Models;
+using Extensions.Hosting.AsyncInitialization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+namespace AlphaBlogging.Services
+{
+    public class DbInitializer : IAsyncInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly bool InitializeDb = true;
+
+        public DbInitializer(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            _db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task InitializeAsync()
+        {
+            //Creates the database if it does not exit and applies any pending migrations
+            await _db.Database.MigrateAsync();
+
+            //if true, initializes the database with some sample data
+            if (InitializeDb)
+            {
+                await CreateRoleAsync("Admin");
+            }
+           
+        }
+
+        private async Task CreateRoleAsync(string roleName)
+        {
+            if (!await _roleManager.RoleExistsAsync(roleName))
+            {
+                var role = new IdentityRole
+                {
+                    Name = roleName
+                };
+                await _roleManager.CreateAsync(role);
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,6 @@
 using AlphaBlogging.Data;
+using AlphaBlogging.Models;
+using AlphaBlogging.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,9 +34,20 @@ namespace AlphaBlogging
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            //Adding DbInitializer Service
+            services.AddAsyncInitializer<DbInitializer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
