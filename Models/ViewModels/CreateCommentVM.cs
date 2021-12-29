a6ce58f365e751ce392126e8195333595f877939
chi.k.larsson@gmail.com <chi.k.larsson@gmail.com>
2021-12-29 08:47:35 +0100

    fix character count on comment, create blog edit page

--- Models/ViewModels/CreateCommentVM.cs
+++ Models/ViewModels/CreateCommentVM.cs
@@ -5,12 +5,16 @@ namespace AlphaBlogging.Models.ViewModels
 {
     public class CreateCommentVM
     {
+        [Required]
         public string Body { get; set; }
 
+        [Required]
         public DateTime Created { get; set; }
 
+        [Required]
         public int AuthorId { get; set; }
 
+        [Required]
         public int PostId { get; set; }
     }
 }
