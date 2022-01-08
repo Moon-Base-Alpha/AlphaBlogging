739e9122332fe51009c91140b0fe84822710aad9
chi.k.larsson@gmail.com <chi.k.larsson@gmail.com>
2022-01-08 09:09:31 +0100

    fixed some css and edit post

--- Models/Post.cs
+++ Models/Post.cs
@@ -30,7 +30,7 @@ namespace AlphaBlogging.Models
 
         [Required]
         public DateTime Created { get; set; }
-
+        [Required]
         public DateTime Updated { get; set; }
 
         public int BlogId { get; set; }
